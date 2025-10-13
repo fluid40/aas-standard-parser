9528a391f80447c84a0d7c539fbf94d6f9f2e0b7
Daniel Klein <daniel.klein@em.ag>
2025-10-13 09:40:13 +0000

    fix: Update version to 0.1.6 and clean up imports in __init__.py
    Signed-off-by: Daniel Klein <daniel.klein@em.ag>

--- aas_standard_parser/__init__.py
+++ aas_standard_parser/__init__.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import importlib.metadata
+from datetime import datetime
 
 # TODO: introduce MIT license
 __copyright__ = f"Copyright (C) {datetime.now().year} :em engineering methods AG. All rights reserved."
@@ -9,12 +9,11 @@ try:
     __version__ = importlib.metadata.version(__name__)
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0-dev"
-    
+
 __project__ = "aas-standard-parser"
 __package__ = "aas-standard-parser"
 
-from aas_standard_parser.aimc_parser import AIMCParser
 from aas_standard_parser.aid_parser import AIDParser
-
+from aas_standard_parser.aimc_parser import AIMCParser
 
 __all__ = ["AIMCParser", "AIDParser"]
