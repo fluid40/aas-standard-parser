cf79a9667d6f91c587747b7e8ca6baac457aefb0
Daniel Klein <daniel.klein@em.ag>
2025-10-21 12:08:40 +0000

    Refactor AIMC parser tests for improved structure and clarity
    Signed-off-by: Daniel Klein <daniel.klein@em.ag>

--- tests/test_aimc_parser.py
+++ tests/test_aimc_parser.py
@@ -1,6 +1,7 @@
 import pytest
 from basyx.aas import model
 
+from aas_standard_parser.aimc_parser import MappingConfiguration
 import aas_standard_parser.aimc_parser as aimc_parser
 from aas_standard_parser.utils import create_submodel_from_file
 
@@ -25,10 +26,24 @@ def test_002_get_mapping_configuration_elements(aimc_submodel: model.submodel):
 
     assert configuration_elements is not None
     assert len(configuration_elements) == 1
-    assert isinstance(configuration_elements[0], model.SubmodelElementCollection)
 
+    configuration_element = configuration_elements[0]
+    assert isinstance(configuration_element, model.SubmodelElementCollection)
 
-def test_003_parse_mapping_configurations(aimc_submodel: model.submodel):
+def test_003_parse_mapping_configuration_element(aimc_submodel: model.submodel):
+    configuration_elements = aimc_parser.get_mapping_configuration_elements(
+        aimc_submodel
+    )
+
+    configuration_element = configuration_elements[0]
+    configuration = aimc_parser.parse_mapping_configuration(configuration_element)
+
+    assert configuration is not None
+    _check_interface_ref(configuration)
+    _check_relations_aid(configuration)
+    _check_relations(configuration)
+
+def test_004_parse_mapping_configurations(aimc_submodel: model.submodel):
     mapping_configurations = aimc_parser.parse_mapping_configurations(aimc_submodel)
 
     assert mapping_configurations is not None
@@ -38,32 +53,39 @@ def test_003_parse_mapping_configurations(aimc_submodel: model.submodel):
     configuration = mapping_configurations.configurations[0]
     assert configuration.aid_submodel_id is not None
     assert configuration.aid_submodel_id in mapping_configurations.aid_submodel_ids
+    assert configuration.aid_submodel_id == "https://fluid40.de/ids/sm/4757_4856_8464_1441"
 
-def test_003a_parse_mapping_configurations_interface_ref(aimc_submodel: model.submodel  ):
-    mapping_configurations = aimc_parser.parse_mapping_configurations(aimc_submodel)
-    configuration = mapping_configurations.configurations[0]
+    _check_interface_ref(configuration)
+    _check_relations_aid(configuration)
+    _check_relations(configuration)
 
+def _check_interface_ref(configuration: MappingConfiguration):
     assert configuration.interface_reference is not None
 
-def test_003b_parse_mapping_configurations_relations_aid(aimc_submodel: model.submodel  ):
-    mapping_configurations = aimc_parser.parse_mapping_configurations(aimc_submodel)
-    configuration = mapping_configurations.configurations[0]
+    interface_reference = configuration.interface_reference
+    assert isinstance(interface_reference, model.ReferenceElement)
+    assert interface_reference.id_short == "InterfaceReference"
 
+    value = interface_reference.value
+    assert isinstance(value, model.Reference)
+    assert len(value.key) == 1
+
+    key = value.key[0]
+    assert key is not None
+    assert key.value == "https://fluid40.de/ids/sm/4757_4856_8464_1442/Interface_MQTT"
+
+def _check_relations_aid(configuration: MappingConfiguration):
     assert len(configuration.source_sink_relations) > 0
 
     relation = configuration.source_sink_relations[0]
     assert relation is not None
     assert relation.aid_submodel_id == configuration.aid_submodel_id
-    assert relation.aid_submodel_id in mapping_configurations.aid_submodel_ids
 
     assert relation.property_name is not None
     assert relation.property_name == "HandlingC"
 
 
-def test_003c_parse_mapping_configurations_relations(aimc_submodel: model.submodel  ):
-    mapping_configurations = aimc_parser.parse_mapping_configurations(aimc_submodel)
-    configuration = mapping_configurations.configurations[0]
-
+def _check_relations(configuration: MappingConfiguration):
     assert len(configuration.source_sink_relations) > 0
 
     relation = configuration.source_sink_relations[0]
